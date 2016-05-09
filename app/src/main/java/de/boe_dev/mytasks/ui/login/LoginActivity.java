0d86f059053c7b13fc731df23eb25067f899f899
Benny Özcetin <info@boe-dev.de>
2016-05-09 12:49:18 +0200

    Update LoginActivity.java
    Add Firebaselogin via mail

--- app/src/main/java/de/boe_dev/mytasks/ui/login/LoginActivity.java
+++ app/src/main/java/de/boe_dev/mytasks/ui/login/LoginActivity.java
@@ -19,6 +19,8 @@ import de.boe_dev.mytasks.ui.MainActivity;
  * Created by benny on 03.05.16.
  */
 public class LoginActivity extends AppCompatActivity {
+    
+    private ProgressDialog mAuthProgressDialog;
 
     @BindView(R.id.login_edit_text_mail) EditText mail_edit_text;
     @BindView(R.id.login_edit_text_password) EditText password_edit_text;
@@ -30,15 +32,72 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         ButterKnife.bind(this);
         Firebase.setAndroidContext(this);
+        
+        mAuthProgressDialog = new ProgressDialog(this);
+        mAuthProgressDialog.setTitle(getString(R.string.progress_dialog_loading));
+        mAuthProgressDialog.setMessage(getString(R.string.progress_dialog_authenticating_with_firebase));
+        mAuthProgressDialog.setCancelable(false);
     }
 
     @OnClick(R.id.login_button)
     public void login(View view) {
-        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+        mAuthProgressDialog.show();
+        Firebase ref = new Firebase(Constants.FIREBASE_URL);
+        ref.authWithPassword(mail_edit_text.getText.toString, password_edit_text.getText().toString),
+            new MyAuthResultHandler(Constants.PASSWORD_PROVIDER)});
+    
+        // startActivity(new Intent(getApplicationContext(), MainActivity.class));
     }
 
     @OnClick(R.id.login_sign_up)
     public void signUp(View view){
         startActivity(new Intent(getApplicationContext(), CreateAccountActivity.class));
     }
+    
+    private class MyAuthResultHandler implements Firebase.AuthResultHandler {
+
+        private final String provider;
+
+        public MyAuthResultHandler(String provider) {
+            this.provider = provider;
+        }
+
+        /**
+         * On successful authentication call setAuthenticatedUser if it was not already
+         * called in
+         */
+        @Override
+        public void onAuthenticated(AuthData authData) {
+            mAuthProgressDialog.dismiss();
+            Log.i(LOG_TAG, provider + " " + getString(R.string.log_message_auth_successful));
+            if (authData != null) {
+                /* Go to main activity */
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intent);
+                finish();
+            }
+        }
+
+        @Override
+        public void onAuthenticationError(FirebaseError firebaseError) {
+            mAuthProgressDialog.dismiss();
+
+            switch (firebaseError.getCode()) {
+                case FirebaseError.INVALID_EMAIL:
+                case FirebaseError.USER_DOES_NOT_EXIST:
+                    mEditTextEmailInput.setError(getString(R.string.error_message_email_issue));
+                    break;
+                case FirebaseError.INVALID_PASSWORD:
+                    mEditTextPasswordInput.setError(firebaseError.getMessage());
+                    break;
+                case FirebaseError.NETWORK_ERROR:
+                    showErrorToast(getString(R.string.error_message_failed_sign_in_no_network));
+                    break;
+                default:
+                    showErrorToast(firebaseError.toString());
+            }
+        }
+    }
+    
 }
